e7fc945d6463b1a26163e51afb41dfc2ac7ddf18
Jakub Olesinski <olesinski.jakub@gmail.com>
2016-08-25 14:08:00 +0200

    recognizer: scene callback added

--- include/ros_recognizer/nodelet/recognizer.h
+++ include/ros_recognizer/nodelet/recognizer.h
@@ -3,6 +3,8 @@
 #define ROS_RECOGNIZER_RECOGNIZER_H
 
 #include <nodelet/nodelet.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <ros/ros.h>
 
 namespace ros_recognizer
 {
@@ -10,7 +12,14 @@ namespace ros_recognizer
 class Recognizer : public nodelet::Nodelet
 {
 public:
+  //TODO: publisher for each step
   virtual void onInit();
+
+  void sceneCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+
+private:
+  ros::NodeHandle node_handle_;
+  ros::Subscriber scene_subscriber_;
 };
 
 }
--- src/nodelet/recognizer.cpp
+++ src/nodelet/recognizer.cpp
@@ -4,7 +4,14 @@
 
 void ros_recognizer::Recognizer::onInit()
 {
-  NODELET_INFO("Initializing nodelet...");
+  NODELET_INFO("Initializing recognizer...");
+  scene_subscriber_ = node_handle_.subscribe("scene", 1, &Recognizer::sceneCallback, this);
+  NODELET_INFO("Initialization done!");
+}
+
+void ros_recognizer::Recognizer::sceneCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+  NODELET_INFO("Received scene cloud! Yippie!");
 }
 
 PLUGINLIB_EXPORT_CLASS(ros_recognizer::Recognizer, nodelet::Nodelet)
