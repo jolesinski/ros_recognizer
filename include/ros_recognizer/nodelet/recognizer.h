f6669d6c657b233648da93c22e9d6a9ef8969fdc
Jakub Olesinski <olesinski.jakub@gmail.com>
2016-08-25 15:02:50 +0200

    recognizer: defined custom services for setting the model

--- include/ros_recognizer/nodelet/recognizer.h
+++ include/ros_recognizer/nodelet/recognizer.h
@@ -5,6 +5,8 @@
 #include <nodelet/nodelet.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <ros/ros.h>
+#include <ros_recognizer/set_model_from_cloud.h>
+#include <ros_recognizer/set_model_from_pcd.h>
 
 namespace ros_recognizer
 {
@@ -16,10 +18,16 @@ public:
   virtual void onInit();
 
   void sceneCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+  bool setModelFromCloud(set_model_from_cloud::Request& request,
+                         set_model_from_cloud::Response& response);
+  bool setModelFromPCD(set_model_from_pcd::Request& request,
+                       set_model_from_pcd::Response& response);
 
 private:
   ros::NodeHandle node_handle_;
   ros::Subscriber scene_subscriber_;
+  ros::ServiceServer model_cloud_service_;
+  ros::ServiceServer model_pcd_service_;
 };
 
 }
--- src/nodelet/recognizer.cpp
+++ src/nodelet/recognizer.cpp
@@ -6,12 +6,30 @@ void ros_recognizer::Recognizer::onInit()
 {
   NODELET_INFO("Initializing recognizer...");
   scene_subscriber_ = node_handle_.subscribe("scene", 1, &Recognizer::sceneCallback, this);
+  model_cloud_service_ = node_handle_.advertiseService("set_model_from_cloud",
+                                                       &Recognizer::setModelFromCloud, this);
+  model_pcd_service_ = node_handle_.advertiseService("set_model_from_pcd",
+                                                     &Recognizer::setModelFromPCD, this);
   NODELET_INFO("Initialization done!");
 }
 
 void ros_recognizer::Recognizer::sceneCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
   NODELET_INFO("Received scene cloud! Yippie!");
+}
+
+bool ros_recognizer::Recognizer::setModelFromCloud(ros_recognizer::set_model_from_cloud::Request& request,
+                                                   ros_recognizer::set_model_from_cloud::Response& response)
+{
+  NODELET_INFO("Received new model cloud! Yayey!");
+  return true;
+}
+
+bool ros_recognizer::Recognizer::setModelFromPCD(ros_recognizer::set_model_from_pcd::Request& request,
+                                                 ros_recognizer::set_model_from_pcd::Response& response)
+{
+  NODELET_INFO("Received new model pcd! Yahoo!");
+  return true;
 }
 
 PLUGINLIB_EXPORT_CLASS(ros_recognizer::Recognizer, nodelet::Nodelet)
