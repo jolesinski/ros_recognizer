7c897dd3fef7165f32f69be7324301049c1f0ece
Jakub Olesinski <olesinski.jakub@gmail.com>
2016-08-28 18:15:09 +0200

    visualize: show model and scene

--- include/ros_recognizer/visualization/visualizer.h
+++ include/ros_recognizer/visualization/visualizer.h
@@ -3,14 +3,35 @@
 #define ROS_RECOGNIZER_VISUALIZER_H
 
 #include <ros_recognizer/common/reconfigurable.h>
+#include <ros_recognizer/preprocessing/local_3d_description.h>
 #include <ros_recognizer/VisualizerConfig.h>
+
+#include <pcl/visualization/pcl_visualizer.h>
 
 namespace ros_recognizer
 {
 
 class Visualizer : public Reconfigurable<VisualizerConfig>
 {
+public:
+  void showModel(const Local3dDescription& model);
+  void showScene(const Local3dDescription& scene);
 
+private:
+  pcl::visualization::PCLVisualizer vis_;
+
+  void showDescription(const ros_recognizer::Local3dDescription& descr,
+                       const std::string& id_prefix);
+
+  void showInput(const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& cloud,
+                 const std::string &id);
+  void showNormals(const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& input,
+                   const pcl::PointCloud<pcl::Normal>::Ptr& normals,
+                   const std::string& id);
+  void showKeypoints(const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& cloud,
+                     const std::string &id);
+
+  Local3dDescription shiftModel(const Local3dDescription& model);
 };
 
 }
--- src/visualization/visualizer.cpp
+++ src/visualization/visualizer.cpp
@@ -1,2 +1,68 @@
 
 #include <ros_recognizer/visualization/visualizer.h>
+#include <pcl/common/transforms.h>
+
+void ros_recognizer::Visualizer::showModel(const ros_recognizer::Local3dDescription& model)
+{
+  auto off_scene_model = shiftModel(model);
+  showDescription(off_scene_model, "model");
+}
+
+void ros_recognizer::Visualizer::showScene(const ros_recognizer::Local3dDescription& scene)
+{
+  showDescription(scene, "scene");
+}
+
+void ros_recognizer::Visualizer::showDescription(const ros_recognizer::Local3dDescription& descr,
+                                                 const std::string& id_prefix)
+{
+  showInput(descr.input_, id_prefix + "_input");
+  showNormals(descr.input_, descr.normals_, id_prefix + "_normals");
+  showKeypoints(descr.keypoints_, id_prefix + "_keypoints");
+}
+
+void ros_recognizer::Visualizer::showInput(const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& cloud,
+                                           const std::string& id)
+{
+  vis_.removePointCloud(id);
+  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> colorInput(cloud);
+  vis_.addPointCloud(cloud, colorInput, id);
+}
+
+void ros_recognizer::Visualizer::showNormals(const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& input,
+                                             const pcl::PointCloud<pcl::Normal>::Ptr& normals,
+                                             const std::string& id)
+{
+  vis_.removePointCloud("normals");
+  vis_.addPointCloudNormals<pcl::PointXYZRGBA, pcl::Normal>(input, normals, 10, 0.02, "normals");
+}
+
+void ros_recognizer::Visualizer::showKeypoints(const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& cloud,
+                                               const std::string& id)
+{
+  vis_.removePointCloud(id);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGBA> colorKeypoints(cloud, 255, 0, 0);
+  vis_.addPointCloud(cloud, colorKeypoints, id);
+  vis_.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 4, id);
+}
+
+ros_recognizer::Local3dDescription
+ros_recognizer::Visualizer::shiftModel(const ros_recognizer::Local3dDescription& model)
+{
+  ros_recognizer::Local3dDescription off_scene_descr;
+
+  pcl::transformPointCloud (*model.input_,
+                            *off_scene_descr.input_,
+                            Eigen::Vector3f(-.5,0,0),
+                            Eigen::Quaternionf(0.5, 0, 0.86603, 0));
+  pcl::transformPointCloud (*model.keypoints_,
+                            *off_scene_descr.keypoints_,
+                            Eigen::Vector3f (-.5,0,0),
+                            Eigen::Quaternionf (0.5, 0, 0.86603, 0));
+
+  off_scene_descr.normals_ = model.normals_;
+  off_scene_descr.descriptors_ = model.descriptors_;
+  off_scene_descr.ref_frames_ = model.ref_frames_;
+
+  return off_scene_descr;
+}
