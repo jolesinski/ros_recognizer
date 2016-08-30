6977595930dddf83eac73e5e5d22a908d61f6ee0
Jakub Olesinski <olesinski.jakub@gmail.com>
2016-08-30 11:41:44 +0200

    viewer: receive corrs and clusters

--- include/ros_recognizer/nodelet/viewer.h
+++ include/ros_recognizer/nodelet/viewer.h
@@ -6,6 +6,7 @@
 #include <geometry_msgs/PoseArray.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <ros/ros.h>
+#include <ros_recognizer/conversions/correspondence_ros.h>
 #include <ros_recognizer/common/dynamic_configurator.h>
 #include <ros_recognizer/visualization/visualizer.h>
 #include <ros_recognizer/conversions/description_ros.h>
@@ -30,10 +31,14 @@ private:
   // Input
   DescriptionSubscriber model_subscriber_;
   DescriptionSubscriber scene_subscriber_;
+  ros::Subscriber corresp_sub;
+  ros::Subscriber cluster_sub;
   ros::Subscriber valid_hyps_sub;
   ros::Subscriber false_hyps_sub;
 
   void initTopics();
+  void setCorresps(const CorrespondenceArrayConstPtr msg);
+  void setClusters(const CorrespondenceClustersConstPtr msg);
   void setValidHyps(const geometry_msgs::PoseArrayPtr msg);
   void setFalseHyps(const geometry_msgs::PoseArrayPtr msg);
 };
--- src/nodelet/viewer.cpp
+++ src/nodelet/viewer.cpp
@@ -30,6 +30,9 @@ void ros_recognizer::Viewer::initTopics()
   model_subscriber_.initTopics(node_handle_, "model");
   scene_subscriber_.initTopics(node_handle_, "scene");
 
+  corresp_sub = node_handle_.subscribe("correspondences", 1, &Viewer::setCorresps, this);
+  cluster_sub = node_handle_.subscribe("clusters", 1, &Viewer::setClusters, this);
+
   valid_hyps_sub = node_handle_.subscribe("valid_hyps", 1, &Viewer::setValidHyps, this);
   false_hyps_sub = node_handle_.subscribe("false_hyps", 1, &Viewer::setFalseHyps, this);
 }
@@ -46,6 +49,18 @@ void ros_recognizer::Viewer::setFalseHyps(const geometry_msgs::PoseArrayPtr msg)
   Hypotheses hyps = convertMsgToHypotheses(*msg, model_subscriber_.getDescription().input_, false);
   NODELET_INFO("Received false hyps: %lu %lu", msg->poses.size(), hyps.size());
   visualizer_.setHypotheses(hyps, false);
+}
+
+void ros_recognizer::Viewer::setCorresps(const ros_recognizer::CorrespondenceArrayConstPtr msg)
+{
+  NODELET_INFO("Received corrs: %lu", msg->correspondences.size());
+  visualizer_.setCorrespondences(toPCL(*msg));
+}
+
+void ros_recognizer::Viewer::setClusters(const ros_recognizer::CorrespondenceClustersConstPtr msg)
+{
+  NODELET_INFO("Received clusters: %lu", msg->clusters.size());
+  visualizer_.setClusters(toPCL(*msg));
 }
 
 PLUGINLIB_EXPORT_CLASS(ros_recognizer::Viewer, nodelet::Nodelet)
